d2e2ce47364781b28685402494fe5047a2ef0615
Posison-L <lichunjin521@qq.com>
2018-04-12 17:20:24 +0800

    low版 Product Management

--- myaddons/zx_product_management/__manifest__.py
+++ myaddons/zx_product_management/__manifest__.py
@@ -1,37 +1,37 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mymodule",
+    'name': "ZX Product Management",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        产品管理系统""",
 
     'description': """
-        Long description of module's purpose
+        产品管理系统，提供产品管理功能。
     """,
 
-    'author': "My Company",
+    'author': "Company",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Test',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['website'],
+    'depends': ['base'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
-        'views/my_templates.xml',
+        'views/workflow.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    # 应用中默认能搜索到请假单模块
     'application': True,
 }
---
+++ myaddons/zx_product_management/models/models.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ProductManagement(models.Model):
+    """
+    Request for absence
+    """
+
+    WORKFLOW_STATE_SELECTIOBN = [
+        ('draft', '草稿'),
+        ('confirm', '待审批'),
+        ('complete', '已完成')
+    ]
+
+    _name = 'product.management'
+
+    name = fields.Char(string="产品名称", required=True)
+    price = fields.Float(string="产品价格",  required=True, )
+    startdate = fields.Date(string="提交日期", required=True)
+    note = fields.Text(string="备注", default=None)
+    state = fields.Selection(WORKFLOW_STATE_SELECTIOBN, default='draft', string='状态',
+                             readonly=True)  # 只读，不可修改
+
+    @api.multi
+    def do_confirm(self):
+        self.state = 'confirm'
+        return True
+
+    @api.multi
+    def do_complete(self):
+        self.state = 'complete'
+        return True
