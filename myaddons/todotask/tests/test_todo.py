7c0ed48ec0f7cd464cdf87deda6210b63b0bc91e
Posison-L <lichunjin521@qq.com>
2018-04-13 15:11:16 +0800

    测试安全访问权限

--- myaddons/todotask/tests/test_todo.py
+++ myaddons/todotask/tests/test_todo.py
@@ -4,9 +4,11 @@
 # Date: 2018/4/13 0013
 
 from odoo.tests.common import TransactionCase
+from odoo.exceptions import AccessError
 
 
 class TestTodo(TransactionCase):
+
     def test_create(self):
         """
         Create a simple Todo
@@ -14,3 +16,26 @@ class TestTodo(TransactionCase):
         Todo = self.env['todo.task']
         task = Todo.create({'name': 'Test Task'})
         self.assertEqual(task.is_done, False)
+
+        # Test Toggle Done
+        task.do_toggle_done()
+        self.assertTrue(task.is_done)
+        # Test Clear Done
+        Todo.do_clear_done()
+        self.assertFalse(task.active)
+
+    def setUp(self, *args, **kwargs):
+        result = super(TestTodo, self).setUp(*args, **kwargs)
+        user_demo = self.env.ref('base.user_demo')
+        self.env = self.env(user=user_demo)
+        return result
+
+    def test_record_rule(self):
+        """
+        测试每个用户记录规则
+        """
+        Todo = self.env['todo.task']
+        task = Todo.sudo().create({'name': 'Admin Task'})
+        with self.assertRaises(AccessError):
+            Todo.browse([task.id])
+
