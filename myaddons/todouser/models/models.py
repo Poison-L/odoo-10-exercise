f51e6d99ef22ee616de007d445462a49f7336287
Posison-L <lichunjin521@qq.com>
2018-04-13 17:01:40 +0800

    修改函数方法及装饰器

--- myaddons/todouser/models/models.py
+++ myaddons/todouser/models/models.py
@@ -1,15 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
-# class todouser(models.Model):
-#     _name = 'todouser.todouser'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class TodoTask(models.Model):
+    _inherit = 'todo.task'
+
+    user_id = fields.Many2one('res.users', 'Responsible')
+    date_deadline = fields.Date('Deadline')
+    name = fields.Char(help="What needs to be done?")
+
+    @api.one
+    def do_toggle_done(self):
+        if self.user_id != self.env.user:
+            raise Exception('Only the responsible can do this!')
+        else:
+            return super(TodoTask, self).do_toggle_done()
+
+    @api.multi
+    def do_clear_done(self):
+        # 波兰表达式
+        domain = [('is_done', '=', True),
+                  '|', ('user_id', '=', self.env.uid),
+                  ('user_id', '=', False)]
+        done_recs = self.search(domain)
+        done_recs.write({'active': False})
+        return True
