93f587d3b80011b8cb9c65add7558f6898a32111
Robert <lichunjin521@gmail.com>
2018-04-14 15:09:36 +0800

    修改 _description = u'To-do Task'

--- myaddons/todotask/models/models.py
+++ myaddons/todotask/models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 
 class TodoTask(models.Model):
     _name = 'todo.task'
-    _description = 'To-do Task'
+    _description = u'To-do Task'
 
     name = fields.Char('Description', required=True)
     is_done = fields.Boolean('Done?', )
