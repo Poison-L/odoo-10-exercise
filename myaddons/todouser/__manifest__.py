5e5ce5215e4fc14e2ba8cbf5eccf2fcbd4cbbb97
Robert <lichunjin521@gmail.com>
2018-04-14 21:33:38 +0800

    'security/todo_access_rules.xml'加入清单data

--- myaddons/todouser/__manifest__.py
+++ myaddons/todouser/__manifest__.py
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'security/todo_access_rules.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
