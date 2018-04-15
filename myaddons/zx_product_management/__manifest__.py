fca367822562be7d3b7d71013bff9833fdafdb07
Posison-L <lichunjin521@qq.com>
2018-04-15 17:25:49 +0800

    depends 删除 zx_product_management模块

--- myaddons/zx_product_management/__manifest__.py
+++ myaddons/zx_product_management/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['zx_product_management', 'mail'],
+    'depends': ['mail'],
 
     # always loaded
     'data': [
