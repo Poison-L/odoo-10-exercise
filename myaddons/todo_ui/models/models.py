f94ea0064f9c76d6eb15a1d5b7740da60757f6c2
Posison-L <lichunjin521@qq.com>
2018-04-16 16:20:35 +0800

    定义标签、状态模型

--- myaddons/todo_ui/models/models.py
+++ myaddons/todo_ui/models/models.py
@@ -7,7 +7,7 @@ class Tag(models.Model):
     _name = 'todo.task.tag'
     _description = 'To-do Tag'
 
-    name = fields.Char('Name', 40, translate=True)
+    name = fields.Char('标签', size=40, translate=True)
 
 
 class Stage(models.Model):
@@ -15,5 +15,29 @@ class Stage(models.Model):
     _description = u'To-do Stage'
     _order = 'sequence,name'
 
-    name = fields.Char('Name', 40, translate=True)
-    sequence = fields.Integer('Sequence')
+    # String fields:
+    name = fields.Char(string='名称', size=40, translate=True)
+    desc = fields.Text('描述')
+    state = fields.Selection(
+        [('draft', 'New'), ('open', 'Started'),
+         ('done', 'Closed')], '状态')
+    docs = fields.Html('文件')
+
+    # Numeric fields:
+    sequence = fields.Integer('序列')
+    perc_complete = fields.Float('% Complete', (3, 2))
+
+    # Date fields:
+    date_effective = fields.Date('有效日期')
+    date_changed = fields.Datetime('最后修改时间')
+
+    # Other fields:
+    fold = fields.Boolean('折叠?')
+    image = fields.Binary('图片')
+
+
+class TodoTask(models.Model):
+    _inherit = 'todo.task'
+
+    stage_id = fields.Many2one('todo.task.stage', 'Stage')
+    tag_ids = fields.Many2many('todo.task.tag', string='Tags')
