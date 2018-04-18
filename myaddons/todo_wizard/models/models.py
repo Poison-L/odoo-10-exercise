87bf8dd395e388c3db89a1870cace1817ecc6496
Robert <lichunjin521@gmail.com>
2018-04-18 22:52:42 +0800

    添加向导业务逻辑

--- myaddons/todo_wizard/models/models.py
+++ myaddons/todo_wizard/models/models.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import logging
 
 from odoo import models, fields, api
+from odoo import exceptions
+
+_logger = logging.getLogger(__name__)
 
 
 class TodoWizard(models.TransientModel):
@@ -11,3 +15,21 @@ class TodoWizard(models.TransientModel):
     new_deadline = fields.Date('Deadline to Set')
     # res.users 是？？？
     new_user_id = fields.Many2one('res.users', string='Responsible to Set')
+
+    @api.multi
+    def do_mass_update(self):
+        self.ensure_one()
+
+        if not (self.new_deadline or self.new_user_id):
+            raise exceptions.ValidationError('No data to update!')
+        _logger.debug('Mass update on Todo Tasks %s',
+                      self.task_ids.ids)
+        vals = {}
+        if self.new_deadline:
+            vals['date_deadline'] = self.new_deadline
+        if self.new_user_id:
+            vals['user_id'] = self.new_user_id
+        # Mass write values on all selected tasks
+        if vals:
+            self.task_ids.write(vals)
+        return True
