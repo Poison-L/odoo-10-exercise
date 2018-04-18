f0cb8e393e1e0ec9ce4ab098ad6c092d1ecd05cf
Robert <lichunjin521@gmail.com>
2018-04-18 22:31:17 +0800

    创建工作计划向导模型

--- myaddons/todo_wizard/models/models.py
+++ myaddons/todo_wizard/models/models.py
@@ -2,14 +2,12 @@
 
 from odoo import models, fields, api
 
-# class todo_wizard(models.Model):
-#     _name = 'todo_wizard.todo_wizard'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class TodoWizard(models.TransientModel):
+    _name = 'todo.wizard'
+    _description = '工作计划向导'
+
+    task_ids = fields.Many2many('todo.task', string='Tasks')
+    new_deadline = fields.Date('Deadline to Set')
+    # res.users 是？？？
+    new_user_id = fields.Many2one('res.users', string='Responsible to Set')
