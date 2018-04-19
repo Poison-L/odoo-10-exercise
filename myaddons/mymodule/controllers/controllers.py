a4c3450eb1a7f669cdbe9869b0b5bac254f08593
Posison-L <lichunjin521@qq.com>
2018-04-19 15:29:00 +0800

    还原恢复

--- myaddons/mymodule/controllers/controllers.py
+++ myaddons/mymodule/controllers/controllers.py
@@ -2,19 +2,19 @@
 from odoo import http
 
 
-class MyBlog(http.Controller):
-    @http.route('/blog/', auth='public')
-    def index(self, **kw):
-        return "1313131313131"
-
-# class Mymodule(http.Controller):
-#     @http.route('/my/fruits/', auth='public', website=True)
+# class MyBlog(http.Controller):
+#     @http.route('/blog/', auth='public')
 #     def index(self, **kw):
-#         # return "Hello, world"
-#
-#         fruits = http.request.env['my.fruits']
-#
-#         return http.request.render('mymodule.index',
-#                                    {'fruits': fruits.search([])})
+#         return "1313131313131"
+
+class Mymodule(http.Controller):
+    @http.route('/my/fruits/', auth='public', website=True)
+    def index(self, **kw):
+        # return "Hello, world"
+
+        fruits = http.request.env['my.fruits']
+
+        return http.request.render('mymodule.index',
+                                   {'fruits': fruits.search([])})
 
 
--- myaddons/mymodule/models/models.py
+++ myaddons/mymodule/models/models.py
@@ -3,14 +3,14 @@
 from odoo import models, fields, api
 
 
-class Blog(models.Model):
-    _name = 'my.blog'
-    _description = u'论坛'
-
-    name = fields.Char(string="名称", required=False, )
-
-
-# class Fruits(models.Model):
-#     _name = 'my.fruits'   # 模块名_对象名
+# class Blog(models.Model):
+#     _name = 'my.blog'
+#     _description = u'论坛'
 #
-#     name = fields.Char(string="水果名称", required=False, )
+#     name = fields.Char(string="名称", required=False, )
+
+
+class Fruits(models.Model):
+    _name = 'my.fruits'   # 模块名_对象名
+
+    name = fields.Char(string="水果名称", required=False, )
