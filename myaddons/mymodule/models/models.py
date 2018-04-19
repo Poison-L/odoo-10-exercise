d04f618a1725d4e395d17811f0c747e5ae69c7f5
Posison-L <lichunjin521@qq.com>
2018-04-19 15:11:52 +0800

    添加blog模型

--- myaddons/mymodule/models/models.py
+++ myaddons/mymodule/models/models.py
@@ -2,20 +2,15 @@
 
 from odoo import models, fields, api
 
-# class mymodule(models.Model):
-#     _name = 'mymodule.mymodule'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
+class Blog(models.Model):
+    _name = 'my.blog'
+    _description = u'论坛'
+
+    name = fields.Char(string="名称", required=False, )
+
+
+# class Fruits(models.Model):
+#     _name = 'my.fruits'   # 模块名_对象名
 #
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
-
-
-class Fruits(models.Model):
-    _name = 'mymodule.fruits'   # 模块名_对象名
-
-    name = fields.Char(string="水果名称", required=False, )
+#     name = fields.Char(string="水果名称", required=False, )
