a2c919a5d7f879c88c102453265216b2f00a0e4e
Posison-L <lichunjin521@qq.com>
2018-04-19 09:17:26 +0800

    修改清单依赖模块

--- myaddons/todo_wizard/__manifest__.py
+++ myaddons/todo_wizard/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['todo_user'],
+    'depends': ['todouser'],
 
     # always loaded
     'data': [
