c8e7b7788c8529ccda9bc21b421e74737b9d66ee
Posison-L <lichunjin521@qq.com>
2018-04-19 15:12:50 +0800

    路由修改

--- myaddons/mymodule/controllers/controllers.py
+++ myaddons/mymodule/controllers/controllers.py
@@ -2,25 +2,19 @@
 from odoo import http
 
 
-class Mymodule(http.Controller):
-    @http.route('/mymodule/mymodule/', auth='public', website=True)
+class MyBlog(http.Controller):
+    @http.route('/blog/', auth='public')
     def index(self, **kw):
-        # return "Hello, world"
+        return "1313131313131"
 
-        fruits = http.request.env['mymodule.fruits']
+# class Mymodule(http.Controller):
+#     @http.route('/my/fruits/', auth='public', website=True)
+#     def index(self, **kw):
+#         # return "Hello, world"
+#
+#         fruits = http.request.env['my.fruits']
+#
+#         return http.request.render('mymodule.index',
+#                                    {'fruits': fruits.search([])})
 
-        return http.request.render('mymodule.index',
-                                   {'fruits': fruits.search([])})
 
-#     @http.route('/mymodule/mymodule/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('mymodule.listing', {
-#             'root': '/mymodule/mymodule',
-#             'objects': http.request.env['mymodule.mymodule'].search([]),
-#         })
-
-#     @http.route('/mymodule/mymodule/objects/<model("mymodule.mymodule"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('mymodule.object', {
-#             'object': obj
-#         })
