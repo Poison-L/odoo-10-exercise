78f8c5ff23021a5e9d88f69bd739cdbe271880cf
Robert <lichunjin521@gmail.com>
2018-07-05 01:21:07 +0800

    尝试jinja2模板引擎

--- myaddons/qingjia/controllers/controllers.py
+++ myaddons/qingjia/controllers/controllers.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+import os
+import sys
+from odoo import http, models, SUPERUSER_ID
+from jinja2 import Environment
+from jinja2 import PackageLoader
+from jinja2 import FileSystemLoader
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+# 加载文件
+templateLoader = FileSystemLoader(searchpath=BASE_DIR + "/templates")
+env = Environment(loader=templateLoader, autoescape=True)
+
+
+class Test(http.Controller):
+
+    @http.route()
+    def foo(self, **kwargs):
+        pass
+        datalist = []
+        template = env.get_template("index.html")
+        # 加载一个模板: template = env.get_template(‘mytemplate.txt’), 使用env的模板环境加载名为#mytemplate.txt的模板文件.
+        return template.render(data=datalist)  # datalist是待传入数据，比如列表
+    # 渲染一个模板: template.render(name = ‘Jack’), 渲染模板template, 传入了模板参数name值为Jack
 
 # class Qingjia(http.Controller):
 #     @http.route('/qingjia/qingjia/', auth='public')
